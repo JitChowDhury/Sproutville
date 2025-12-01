59e3ed0fc2cb22ee01882cb0c5dcfe7f37c40881
JitChowDhury <jitchowdhury8717@gmail.com>
2025-12-01 15:15:21 +0530

    Set Up Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private InputActionReference moveInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput.action.ReadValue<Vector2>() * moveSpeed;
+    }
+}
