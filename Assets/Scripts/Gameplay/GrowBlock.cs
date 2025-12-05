16e7c8b27179d1a7bde7f9b125379a86d3f1a723
JitChowDhury <jitchowdhury8717@gmail.com>
2025-12-05 22:56:04 +0530

    Showing Growth

--- Assets/Scripts/Gameplay/GrowBlock.cs
+++ Assets/Scripts/Gameplay/GrowBlock.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GrowBlock : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private SpriteRenderer theSR;
+    [SerializeField] private Sprite soilTilled;
+    public enum GrowthStage
+    {
+        barren,
+        ploughed,
+        planted,
+        growing1,
+        growing2,
+        ripe
+    }
+
+    private GrowthStage currentStage;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            AdvanceStage();
+
+            SetSoilSprite();
+        }
+
+    }
+
+    void AdvanceStage()
+    {
+        currentStage = currentStage + 1;
+        if ((int)currentStage >= 6)
+        {
+            currentStage = GrowthStage.barren;
+        }
+    }
+
+    public void SetSoilSprite()
+    {
+        if (currentStage == GrowthStage.barren)
+        {
+            theSR.sprite = null;
+        }
+        else
+            theSR.sprite = soilTilled;
     }
 }
